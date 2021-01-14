d8900438c0d8cb8c3d879aff12936eca1b56b365
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-01-14 11:45:30 +0530

    Added UI for Taking photo or from camera in home.dart file

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,10 +18,10 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
-              height: 50,
+              height: 85,
             ),
             Text(
-              "Teachable Convulation Neural Network",
+              "App  Convulation Neural Network",
               style: TextStyle(color: Color(0xFFEEDA28), fontSize: 15),
             ),
             SizedBox(
@@ -35,7 +37,64 @@ class _HomeState extends State<Home> {
             SizedBox(
               height: 40,
             ),
-            
+            Center(
+              child: _loading
+                  ? Container(
+                      child: Column(
+                        children: [
+                          Image.asset("assets/cat.png"),
+                          SizedBox(
+                            height: 50,
+                          )
+                        ],
+                      ),
+                    )
+                  : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE99600),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        "Take a Photo",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE99600),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        "Camera",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
